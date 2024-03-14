fc9dce263e5ed27e788a6ff97d0c82ea680413c8
philoniare <philoniare@gmail.com>
2024-03-14 13:56:12 +0800

    feat: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -16,52 +16,48 @@ mod tests;
 // All pallet logic is defined in its own module and must be annotated by the `pallet` attribute.
 #[frame_support::pallet]
 pub mod pallet {
-	// Import various useful types required by all FRAME pallets.
-	use super::*;
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+    // Import various useful types required by all FRAME pallets.
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
 
-	// The `Pallet` struct serves as a placeholder to implement traits, methods and dispatchables
-	// (`Call`s) in this pallet.
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    // The `Pallet` struct serves as a placeholder to implement traits, methods and dispatchables
+    // (`Call`s) in this pallet.
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-	/// The pallet's configuration trait.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// The overarching runtime event type.
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-	}
+    /// The pallet's configuration trait.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching runtime event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    }
 
-	/// A storage item for this pallet.
-	#[pallet::storage]
-	pub type SomeItem<T> = StorageValue<_, u32>;
+    /// A storage item for this pallet.
+    #[pallet::storage]
+    pub type SomeItem<T> = StorageValue<_, u32>;
 
-	/// A storage map for this pallet.
-	#[pallet::storage]
-	pub type SomeMap<T> = StorageMap<_, Blake2_128Concat, u32, u32>;
+    /// A storage map for this pallet.
+    #[pallet::storage]
+    pub type SomeMap<T> = StorageMap<_, Blake2_128Concat, u32, u32>;
 
-	/// Events that functions in this pallet can emit.
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/* Pallet Event Variants Go Here */
-	}
+    /// Events that functions in this pallet can emit.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {/* Pallet Event Variants Go Here */}
 
-	/// Errors that can be returned by this pallet.
-	#[pallet::error]
-	pub enum Error<T> {
-		/* Pallet Error Variants Go Here */
-	}
+    /// Errors that can be returned by this pallet.
+    #[pallet::error]
+    pub enum Error<T> {/* Pallet Error Variants Go Here */}
 
-	/// The pallet's dispatchable functions ([`Call`]s).
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/* User Callable Functions Go Here */
-	}
+    /// The pallet's dispatchable functions ([`Call`]s).
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /* User Callable Functions Go Here */
+    }
 
-	/// The pallet's internal functions.
-	impl<T: Config> Pallet<T> {
-		/* Internally Callable Functions Go Here */
-	}
+    /// The pallet's internal functions.
+    impl<T: Config> Pallet<T> {
+        /* Internally Callable Functions Go Here */
+    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,9 +3,9 @@ use frame_support::{assert_noop, assert_ok};
 
 #[test]
 fn basic_test() {
-	new_test_ext().execute_with(|| {
-		// Go past genesis block so events get deposited
-		System::set_block_number(1);
-		// Future test conditions would go here.
-	});
+    new_test_ext().execute_with(|| {
+        // Go past genesis block so events get deposited
+        System::set_block_number(1);
+        // Future test conditions would go here.
+    });
 }
